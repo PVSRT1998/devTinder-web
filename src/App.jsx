c7fdb0bb7846be2ba5f269e2903f14bb56281c8b
Venkata Teja <pvsreerangateja1998@gmail.com>
2026-08-11 22:21:10 +0530

    learning-Initated project with layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Body from './Body'
+import Login from './Login'
+import Profile from './Profile'
 
 function App() {
 
   return (
     <>
-      <h1> Hello World!</h1>
+
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />} >
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
