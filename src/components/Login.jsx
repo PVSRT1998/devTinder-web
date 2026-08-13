aa47dd7d9ea524a068d21a981f00d9b4b8bb765f
Venkata Teja <pvsreerangateja1998@gmail.com>
2026-08-13 10:58:51 +0530

    learning-1208

---
+++ src/components/Login.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+
+  const [emailId, setEmailId] = useState('venkatateja@gmail.com');
+  const [password, setPassword] = useState('Venkata@123');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+    // Handle login logic here
+    const res = await axios.post('http://localhost:3001/login', { emailId, password }, {withCredentials: true});
+    console.log(res);
+    dispatch(addUser(res.data.data));
+    navigate("/");
+    navi
+    } catch(err) {
+      console.log("ERROR: " + err.message);
+    }
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center my-10">
+      <div className="card card-border bg-base-300 w-96">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <fieldset className="fieldset">
+              <label className="label" htmlFor="email">Email ID</label>
+              <input type="email" id="email" className="input" placeholder="Email ID" value={emailId} onChange={(e) => setEmailId(e.target.value)} />
+            </fieldset>
+            <fieldset className="fieldset">
+              <label className="label" htmlFor="password">Password</label>
+              <input type="password" id="password" className="input" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const NavBar = () => {
+
+  const user = useSelector(store => store.user);
+  console.log(user);
+
   return (
     <div>
       <div className="navbar bg-base-300 shadow-sm">
         <div className="flex-1">
           <a className="btn btn-ghost text-xl">DevTinder</a>
         </div>
-        <div className="flex gap-2">
+
+        {user && <div className="flex gap-2">
+          <p className='flex justify-center'>Welcome, {user.firstName}</p>
           <div className="dropdown dropdown-end">
             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
@@ -29,7 +36,7 @@ const NavBar = () => {
               <li><a>Logout</a></li>
             </ul>
           </div>
-        </div>
+        </div>}
       </div>
     </div>
   )
